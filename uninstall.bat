4079dc9eec9f2876680baeed6b0fcf72427eb94a
Ilya Ivanov <forn@ngs.ru>
2014-12-19 03:42:15 +0000

    be more verbose about uninstallation

--- uninstall.bat
+++ uninstall.bat
@@ -1,18 +1,18 @@
 @echo off
-
-call :restore > tools/logs/uninstall.log 2>&1
+echo Uninstall Fallout 2 unlimited party mod? (ctrl-c to cancel)
+pause
+call :restore >tools/logs/uninstall.log 2>&1
+echo Uninstalled!
+pause
 goto :exit
 
 
 :restore
-@echo on
-echo "version 5"
 pushd ..
 set "game_dir=%CD%"
 popd
 set "backup_dir=tools\backup"
 set "scripts_dir=%game_dir%\data\Scripts"
 move /y "%backup_dir%"\*.int "%scripts_dir%"
-@echo off
 
 :exit
