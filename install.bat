d82c191aebfac0a892d5fb17abc0cbc0854a62fc
Ilya Ivanov <forn@ngs.ru>
2014-12-19 03:44:18 +0000

    fix CHA 2 bug, fix some gema warnings, be more verbose during installation

--- install.bat
+++ install.bat
@@ -1,12 +1,19 @@
 @echo off
-
-call :process_files > tools/logs/install.log 2>&1
-goto :exit
-
+echo Unlimited party mod for Fallout 2
+echo Version 6
+echo This mod allows you to recruit companions regardless of your Charisma stat
+echo Please direct feedback to http://github.com/burner1024/fallout2-unlimited-party-mod/issues
+pause
+echo Prosessing scripts...
+call :process_files 2>tools/logs/install.log
+echo ...done
+echo Cleaning up...
+call :cleanup >tools/logs/install.log 2>&1
+echo ...done
+pause
+exit
 
 :process_files
-@echo on
-echo "version 5"
 pushd ..
 set "game_dir=%CD%"
 popd
@@ -35,18 +42,20 @@ FOR %%S in (
   wcbrnbot
 ) DO (
   IF EXIST "%scripts_dir%\%%S.int" ( 
+    echo "%%S"
     copy /y "%scripts_dir%\%%S.int" "%backup_dir%"
     copy /y "%scripts_dir%\%%S.int" "%tmp_dir%"
     ruby "%tools_dir%/compiler/decompile" "%tmp_dir%\%%S.int"
-    gema -line -nobackup -p "((op_metarule(16, 0) - 1) >\= (#))=((op_metarule(16, 0) - 1) \>\= 99" -in "%tmp_dir%\%%S.ssl" -out "%tmp_dir%\%%S.ssl.tmp"
-    gema -line -nobackup -p "((op_metarule(16, 0) - 1) >\= *)=((op_metarule(16, 0) - 1) \>\= 99)" -in "%tmp_dir%\%%S.ssl.tmp" -out "%tmp_dir%\%%S.ssl"
+    gema -line -nobackup -p "((op_metarule(16, 0) - 1) \>\= (#))=((op_metarule(16, 0) - 1) >= 99" -in "%tmp_dir%\%%S.ssl" -out "%tmp_dir%\%%S.ssl.tmp"
+    gema -line -nobackup -p "((op_metarule(16, 0) - 1) \>\= *)=((op_metarule(16, 0) - 1) >= 99)" -in "%tmp_dir%\%%S.ssl.tmp" -out "%tmp_dir%\%%S.ssl"
+    ruby -i.bak -pe "gsub /\(op_get_critter_stat\(op_dude_obj\(\), 3\) \<\= 1\)/, '(op_get_critter_stat(op_dude_obj(), 3) <= -99)'" "%tmp_dir%\%%S.ssl"
+    ruby -i.bak -pe "gsub /\(op_get_critter_stat\(op_dude_obj\(\), 3\) \=\= 1\)/, '(op_get_critter_stat(op_dude_obj(), 3) <= -99)'" "%tmp_dir%\%%S.ssl"
     ruby "%tools_dir%\compiler\compile" "%tmp_dir%\%%S.ssl"
   )
 )
+goto: eof
 
+:cleanup
 move /y "%tmp_dir%"\*.int "%scripts_dir%"
 del /f /q /s "%tmp_dir%"\*.*
-@echo off
-
-
-:exit
+goto: eof
